ef69cf05778ecf8789306215fd844f2654d46f4f
Hyun <hyun@balancehero.com>
2021-02-06 20:21:52 +0900

    add

--- javascript/object.js
+++ javascript/object.js
@@ -17,3 +17,18 @@ a[b] = "1";
 
 
 alert(JSON.stringify(a) )
+
+/**
+ copy object or assign object
+ */
+
+const target = { a: 1, b: 2 };
+const source = { b: 4, c: 5 };
+
+const returnedTarget = Object.assign(target, source);
+
+console.log(target);
+// expected output: Object { a: 1, b: 4, c: 5 }
+
+console.log(returnedTarget);
+// expected output: Object { a: 1, b: 4, c: 5 }
