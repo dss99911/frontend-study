b099512419640d31a7e605a3670427e88cb545cc
Hyun <hyun@balancehero.com>
2021-02-27 16:48:56 +0900

    add

---
+++ bootstrap/grid.md
@@ -0,0 +1,1 @@
+https://www.w3schools.com/bootstrap/bootstrap_grid_basic.asp
--- javascript/list & array.js
+++ javascript/list & array.js
@@ -1,6 +1,13 @@
 const numbers = [1, 2, 3, 4, 5];
-const doubled = numbers.map((number) => number * 2);
+const doubled = numbers.map(number => number * 2);
 
 const nextUsers = [ ...users ]; // users 배열을 복사하고
-
+const distinctNumber = [...new Set(numbers)]
 console.log(doubled);
+
+var fruits = ["apple", "orange", "cherry"];
+fruits.forEach(myFunction);
+
+function myFunction(item, index) {
+    document.getElementById("demo").innerHTML += index + ":" + item + "<br>";
+}
