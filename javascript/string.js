ab1253641c0470ecb3afc5c8278eb3b84ecdc24e
Hyun <hyun@balancehero.com>
2021-03-11 23:05:50 +0900

    add

--- javascript/string.js
+++ javascript/string.js
@@ -2,3 +2,8 @@
 let text = "https://m.shopclues.com/lifestyle-sale-11.html?bsid%5b%5d=139&bsid%5b%5d=44419&bsid%5b%5d=175&bsid%5b%5d=504&bsid%5b%5d=186&bsid%5b%5d=419"
 let number = text.indexOf("?");
 text.substring(0, number)
+
+let rawString = `sdafasdf
+asdfasd""\`
+${text}
+asdf`
