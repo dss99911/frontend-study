4aa27cfb2090c5bdead5c5769b4c594576e0641e
Hyun <hyun@balancehero.com>
2021-03-13 01:02:59 +0900

    add

--- javascript/list & array.js
+++ javascript/list & array.js
@@ -1,5 +1,11 @@
 const numbers = [1, 2, 3, 4, 5];
 const doubled = numbers.map(number => number * 2);
+const doubled = numbers.flatMap(number => [number, number]);
+const doubled = numbers.filter(number => number);
+const doubled = numbers.filter(number => {return number});
+
+//merge arrays
+const twoNumbers = numbers.concat(numbers)
 
 const nextUsers = [ ...users ]; // users 배열을 복사하고
 const distinctNumber = [...new Set(numbers)]
@@ -7,6 +13,7 @@ console.log(doubled);
 
 var fruits = ["apple", "orange", "cherry"];
 fruits.forEach(myFunction);
+fruits.forEach(a => alert(a));
 
 function myFunction(item, index) {
     document.getElementById("demo").innerHTML += index + ":" + item + "<br>";
